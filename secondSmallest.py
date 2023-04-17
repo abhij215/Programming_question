667dc032550b355470817caf1e645aa7d2a9b389
abc <abc@example.com>
2023-04-17 11:56:40 -0400

    adding additional files

---
+++ secondSmallest.py
@@ -0,0 +1,7 @@
+def ssmallest(self, s, n):
+    
+
+if __name__ == '__main__':
+    S = int(input("Sum of digit"))
+    N = int(input('number of digit'))
+    ssmallest(S,N)
---
+++ test.py
@@ -0,0 +1,13 @@
+
+def numberList(n,s):
+    numlist = [
+        for i in range 
+    ]
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    S = int(input())
+
+    NumberList = numberList(N,S)
